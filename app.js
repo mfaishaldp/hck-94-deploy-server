7e22be1490a51eedc856e16761d79859a393445b
Hacktiv8 <hacktiv8@Hacktiv8s-MacBook-Air.local>
2026-04-07 21:30:21 +0700

    init

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+const HelloController = require("./controllers/HelloController");
+const authRouter = require("./routers/auth");
+const movieRouter = require("./routers/movies");
+const authentication = require("./middlewares/authentication");
+const errorHandler = require("./middlewares/errorHandler");
+
+//! middleware body-parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//! hello endpoint
+app.get("/", HelloController.getHello);
+
+//! user endpoint
+app.use(authRouter);
+
+//! middleware authentication
+app.use(authentication);
+
+//! Movies endpoint
+app.use("/movies", movieRouter);
+// BAD Practices -> app.use(movieRouter)
+// kalo ada middleware yg dipasang didalam movieRouter
+// bakal bocor ke endpoint setelahnya
+
+//! middleware errorHandler dibawah untuk menangkap keseluruhan error
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
